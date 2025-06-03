41824b04c17ca31fe17bd1aa49447601d50ef8ee
Ghazal <ghazal.ghahraei@gmail.com>
2025-06-03 11:08:59 +0200

    vector.py

--- vector.py
+++ vector.py
@@ -3,7 +3,6 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 import os
 
 pdf_files = [
